0d3ebd2fa9f1a0ae056bb47ed166a0648edb56ff
Shawn <shawntennity@gmail.com>
2019-09-28 10:46:35 -0400

    Initial commit

---
+++ example modules/app.js
@@ -0,0 +1,22 @@
+// CommonJS Module Syntax
+// const person = require('./myModule1');
+
+// console.log(person.name);
+
+// ES2015 Module
+// import {
+//   person,
+//   sayHello
+// } from './myModule2';
+// console.log(person.name);
+
+// console.log(sayHello());
+
+// import * as mod from './myModule2';
+// console.log(mod.person.name);
+
+// console.log(mod.sayHello());
+
+import greeting from './myModule2'
+
+console.log(greeting);
---
+++ example modules/myModule2.js
@@ -0,0 +1,11 @@
+export const person = {
+  name: 'Chuck',
+  age: '2'
+}
+
+export function sayHello() {
+  return `Hello ${person.name}`;
+}
+
+const greeting = 'Hello World';
+export default greeting;
